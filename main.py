42c3cf436f77a5f55a155bb6eb85f1f8b933c9f1
LevelWard <noah.jonsson@gmail.com>
2023-11-13 10:20:05 +0100

    Fixed comments and added sampling frequency print

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import numpy as np
-import sounddevice as sd # https://python-sounddevice.readthedocs.io/en/0.4.6/usage.html
-import soundfile as sf # https://pypi.org/project/soundfile/
-import librosa # https://librosa.org/doc/latest/index.html
+import sounddevice as sd #https://python-sounddevice.readthedocs.io/en/0.4.6/usage.html
+import soundfile as sf #https://pypi.org/project/soundfile/
+import librosa #https://librosa.org/doc/latest/index.html
 
 filename = 'Orkester.wav'
-# Extract data and sampling rate from file
+# Extract audio from file. data = sound and fs = Hz.
 data, fs = sf.read(filename, dtype='float32')
+print("Sampling rate: " + str(fs))
 sd.play(data, fs)
 status = sd.wait()  # Wait until file is done playing
 
