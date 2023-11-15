4a3fb1908965d98857507fa86ba4c60635c0819b
Iloonio <decadentlithium@gmail.com>
2023-11-15 14:08:21 +0100

    implemented sumofsamples and splicesample

--- sample_modifying.py
+++ sample_modifying.py
@@ -1,14 +1,14 @@
-def spliceSample(samples_array):
-    # takes in args 'samples_array'
-    #
-    #
-    spliced_array = []
+import numpy as np
+
+
+def spliceSample(samples_array, start, end):
+    # takes in args 'samples_array', 'start', 'end'
+    # returns spliced array
+    spliced_array = samples_array[start:end]
     return spliced_array
 
 
 def sumOfSamples(a, b):
     # takes in args a & b, which are samples of array
-    #
-    #
-    sum_array = []
+    sum_array = np.add(a, b)
     return sum_array
