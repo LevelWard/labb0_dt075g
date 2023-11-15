ec33de3b018be9429bcc81204162f5d6f0cf0cd3
Iloonio <decadentlithium@gmail.com>
2023-11-15 13:04:28 +0100

    synthesize_sound.py added

--- synthesize_sound.py
+++ synthesize_sound.py
@@ -7,7 +7,7 @@ p = pyaudio.PyAudio()
 
 volume = 0.5  # range [0.0, 1.0]
 fs = 44100  # sampling rate, Hz, must be integer
-duration = 2.0  # in seconds, may be float
+duration = 23  # in seconds, may be float
 f = 440.0  # sine frequency, Hz, may be float
 
 # generate samples, note conversion to float32 array
