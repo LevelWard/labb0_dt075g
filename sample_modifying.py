c7dea4a6cfa3cc8cef01c104b7d213c807efdf04
Iloonio <decadentlithium@gmail.com>
2023-11-15 13:36:11 +0100

    added new function files

---
+++ sample_modifying.py
@@ -0,0 +1,14 @@
+def spliceData(samples_array):
+    # takes in args 'samples_array'
+    #
+    #
+    spliced_array = []
+    return spliced_array
+
+
+def sumOfSamples(a, b):
+    #
+    #
+    #
+    sum_array = []
+    return sum_array
