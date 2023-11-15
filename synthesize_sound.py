56ab71c974e298b19656e86fe83aaa5927d8f785
Iloonio <decadentlithium@gmail.com>
2023-11-15 13:13:09 +0100

    synthesized_sound.py is now a function

--- synthesize_sound.py
+++ synthesize_sound.py
@@ -1,33 +1,38 @@
 import time
-
 import numpy as np
 import pyaudio
 
-p = pyaudio.PyAudio()
 
-volume = 0.5  # range [0.0, 1.0]
-fs = 44100  # sampling rate, Hz, must be integer
-duration = 23  # in seconds, may be float
-f = 440.0  # sine frequency, Hz, may be float
+# Returns a numpy array of samples
+def synthesize_sine_sound():
+    # Sine wave audio synthesized.
+    p = pyaudio.PyAudio()
 
-# generate samples, note conversion to float32 array
-samples = (np.sin(2 * np.pi * np.arange(fs * duration) * f / fs)).astype(np.float32)
+    volume = 0.15  # range [0.0, 1.0]
+    fs = 44100  # sampling rate, Hz, must be integer
+    duration = 23  # in seconds, may be float
+    f = 440.0  # sine frequency, Hz, may be float
 
-# per @yahweh comment explicitly convert to bytes sequence
-output_bytes = (volume * samples).tobytes()
+    # generate samples, note conversion to float32 array
+    samples = (np.sin(2 * np.pi * np.arange(fs * duration) * f / fs)).astype(np.float32)
 
-# for paFloat32 sample values must be in range [-1.0, 1.0]
-stream = p.open(format=pyaudio.paFloat32,
-                channels=1,
-                rate=fs,
-                output=True)
+    # per @yahweh comment explicitly convert to bytes sequence
+    output_bytes = (volume * samples).tobytes()
 
-# play. May repeat with different volume values (if done interactively)
-start_time = time.time()
-stream.write(output_bytes)
-print("Played sound for {:.2f} seconds".format(time.time() - start_time))
+    # for paFloat32 sample values must be in range [-1.0, 1.0]
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=fs,
+                    output=True)
 
-stream.stop_stream()
-stream.close()
+    # play. May repeat with different volume values (if done interactively)
+    start_time = time.time()
+    stream.write(output_bytes)
+    print("Played sound for {:.2f} seconds".format(time.time() - start_time))
 
-p.terminate()
+    stream.stop_stream()
+    stream.close()
+
+    p.terminate()
+
+    return samples
